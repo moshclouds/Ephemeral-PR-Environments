91d007a2bc5d1ca9790a08caf2b03166a6373893
Mohammad Shabeer <moshdev2213@gmail.com>
2026-08-07 08:23:51 +0530

    feat: initialize inventory-service with Prisma integration and CRUDs

--- app/inventory-service/src/app.module.ts
+++ app/inventory-service/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { InventoryModule } from './modules/inventory/inventory.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [InventoryModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
