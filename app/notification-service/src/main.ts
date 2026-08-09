d4f816f50a05cbf6d490db891eacce4219e02a8e
Mohammad Shabeer <moshdev2213@gmail.com>
2026-08-09 18:16:33 +0530

    feat: initialize inventory, notification, and order microservices with NestJS, Prisma seeding, and project configuration

--- app/notification-service/src/main.ts
+++ app/notification-service/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = parseInt(process.env.PORT || '3002', 10);
   await app.listen(port, '0.0.0.0');
 }
